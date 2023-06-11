6b3a8892d341c50a3c9963e4cff91fab968398a3
Norboro <ajbrook19@gmail.com>
2023-06-11 11:51:20 -0400

    Clone starter code.
    Configure webpack.
    
    Add scripts to packages.

---
+++ client/src/js/install.js
@@ -0,0 +1,18 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    console.log('beforeinstallprompt fired');
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    console.log('butInstall clicked');
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log('appinstalled fired', event);
+});
